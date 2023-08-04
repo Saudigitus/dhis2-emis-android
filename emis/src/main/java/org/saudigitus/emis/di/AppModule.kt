78f9eeb38359945df698de9873914324e62b8c97
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-08-04 16:55:24 +0200

    implementing app di

---
+++ emis/src/main/java/org/saudigitus/emis/di/AppModule.kt
@@ -0,0 +1,31 @@
+package org.saudigitus.emis.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.dhis2.commons.network.NetworkUtils
+import org.hisp.dhis.android.core.D2
+import org.saudigitus.emis.data.local.DataManager
+import org.saudigitus.emis.data.local.repository.DataManagerImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesNetworkUtils(
+        @ApplicationContext context: Context
+    ): NetworkUtils = NetworkUtils(context)
+
+    @Provides
+    @Singleton
+    fun providesDataManager(
+        d2: D2,
+        networkUtils: NetworkUtils
+    ): DataManager = DataManagerImpl(d2, networkUtils)
+}
