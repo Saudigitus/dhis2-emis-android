2144bc5e201f8057f147f7952ef2292bf26ba91f
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-08-07 09:47:29 +0200

    implementing filter model and state

---
+++ emis/src/main/java/org/saudigitus/emis/ui/teis/FilterState.kt
@@ -0,0 +1,15 @@
+package org.saudigitus.emis.ui.teis
+
+import org.saudigitus.emis.data.model.OU
+import org.saudigitus.emis.ui.components.DropDownItem
+
+data class FilterState(
+    val academicYear: DropDownItem? = null,
+    val school: OU? = null,
+    val grade: DropDownItem? = null,
+    val section: DropDownItem? = null
+) {
+    fun isNull() = academicYear == null &&
+    school == null && grade == null &&
+    section == null
+}
