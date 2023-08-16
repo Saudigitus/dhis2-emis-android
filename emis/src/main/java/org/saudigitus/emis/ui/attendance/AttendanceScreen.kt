4a1b4c56be8c77c406e58fc174e63750dbc2d973
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-08-16 16:45:16 +0200

    implementing attendance ui business logic data

--- emis/src/main/java/org/saudigitus/emis/ui/attendance/AttendanceScreen.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/attendance/AttendanceScreen.kt
@@ -11,29 +11,39 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.delay
 import org.saudigitus.emis.R
 import org.saudigitus.emis.ui.components.MetadataItem
 import org.saudigitus.emis.ui.components.ShowCard
 import org.saudigitus.emis.ui.components.Toolbar
 import org.saudigitus.emis.ui.components.ToolbarActionState
-import timber.log.Timber
+import org.saudigitus.emis.utils.Constants.ABSENT
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,7 +51,6 @@ fun AttendanceScreen(
     viewModel: AttendanceViewModel,
     onBack: () -> Unit
 ) {
-
     val students by viewModel.teis.collectAsStateWithLifecycle()
     val attendanceOptions by viewModel.attendanceOptions.collectAsStateWithLifecycle()
     val attendanceBtnState by viewModel.attendanceBtnState.collectAsStateWithLifecycle()
@@ -49,7 +58,62 @@ fun AttendanceScreen(
     val attendanceStatus by viewModel.attendanceStatus.collectAsStateWithLifecycle()
     val toolbarHeaders by viewModel.toolbarHeaders.collectAsStateWithLifecycle()
     val infoCard by viewModel.infoCard.collectAsStateWithLifecycle()
+    val reasonOfAbsence by viewModel.reasonOfAbsence.collectAsStateWithLifecycle()
+    val absence by viewModel.absenceStateCache.collectAsStateWithLifecycle()
 
+    var isAbsent by remember { mutableStateOf(false) }
+    var isAttendanceCompleted by remember { mutableStateOf(false) }
+
+    var cachedTEIId by remember { mutableStateOf("") }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val context = LocalContext.current
+
+    if (isAbsent) {
+        ReasonForAbsenceDialog(
+            reasons = reasonOfAbsence,
+            title = stringResource(R.string.reason_absence),
+            themeColor = Color(0xFF2C98F0),
+            selectedItemCode = if (absence.find { it.tei == cachedTEIId } != null) {
+                absence.find { it.tei == cachedTEIId }!!.reasonOfAbsence
+            } else null,
+            onItemClick = {
+                viewModel.setAbsence(reasonOfAbsence = it.code)
+            },
+            onCancel = {
+                isAbsent = false
+            },
+            onDone = {
+                isAbsent = false
+                viewModel.saveAbsenceState()
+            }
+        )
+    }
+
+    if (attendanceStep == ButtonStep.SAVING) {
+        AttendanceSummaryDialog(
+            title = stringResource(R.string.attendance_summary),
+            data = viewModel.getSummary(),
+            themeColor = Color(0xFF2C98F0),
+            onCancel = { viewModel.setAttendanceStep(ButtonStep.HOLD_SAVING) }
+        ) {
+            viewModel.clearCache()
+            viewModel.setAttendanceStep(ButtonStep.EDITING)
+            isAttendanceCompleted = true
+        }
+    }
+
+    if (isAttendanceCompleted) {
+        LaunchedEffect(key1 = snackbarHostState) {
+            snackbarHostState.showSnackbar(
+                message = context.getString(R.string.attendance_saved),
+                duration = SnackbarDuration.Short
+            )
+
+            delay(200)
+            onBack.invoke()
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -77,7 +141,11 @@ fun AttendanceScreen(
             ExtendedFloatingActionButton(
                 text = {
                    Text(
-                       text = stringResource(R.string.update),
+                       text = if (attendanceStep == ButtonStep.EDITING) {
+                           stringResource(R.string.update)
+                       } else {
+                           stringResource(R.string.save)
+                       },
                        color = Color(0xFF2C98F0),
                        style = LocalTextStyle.current.copy(
                            fontFamily = FontFamily(Font(R.font.rubik_medium))
@@ -86,13 +154,26 @@ fun AttendanceScreen(
                 },
                 icon = {
                    Icon(
-                       imageVector = Icons.Default.Edit,
+                       imageVector = if (attendanceStep == ButtonStep.EDITING) {
+                           Icons.Default.Edit
+                       } else {
+                           Icons.Default.Save
+                       },
                        contentDescription = null,
                        tint = Color(0xFF2C98F0)
                    )
                 },
-                onClick = { /*TODO*/ }
+                onClick = {
+                    if (attendanceStep == ButtonStep.HOLD_SAVING) {
+                        viewModel.setAttendanceStep(ButtonStep.SAVING)
+                    } else {
+                        viewModel.setAttendanceStep(ButtonStep.HOLD_SAVING)
+                    }
+                }
             )
+        },
+        snackbarHost = {
+            SnackbarHost(snackbarHostState)
         }
     ) { paddingValues ->
         Column(
@@ -131,6 +212,7 @@ fun AttendanceScreen(
                             attrValue = "${
                                 student.attributeValues?.values?.toList()?.get(0)?.value()
                             }",
+                            enableClickAction = false,
                             onClick = {}
                         ) {
                             if (attendanceStep == ButtonStep.EDITING) {
@@ -144,7 +226,23 @@ fun AttendanceScreen(
                                     btnState = attendanceBtnState,
                                     actions = attendanceOptions
                                 ) { index, tei, attendance ->
-                                    Timber.tag("TEI_ATTENDANCE").e(attendance)
+                                    if (attendance != ABSENT) {
+                                        viewModel.setAttendance(
+                                            index = index,
+                                            ou = student.tei.organisationUnit() ?: "",
+                                            tei = tei ?: student.tei.uid(),
+                                            value = attendance
+                                        )
+                                    } else {
+                                        cachedTEIId = tei ?: student.tei.uid()
+                                        isAbsent = true
+                                        viewModel.setAbsence(
+                                            index = index,
+                                            ou = student.tei.organisationUnit() ?: "",
+                                            tei = tei ?: student.tei.uid(),
+                                            value = attendance
+                                        )
+                                    }
                                 }
                             }
                         }
--- emis/src/main/java/org/saudigitus/emis/ui/attendance/AttendanceViewModel.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/attendance/AttendanceViewModel.kt
@@ -11,13 +11,19 @@ import org.dhis2.commons.data.SearchTeiModel
 import org.dhis2.commons.date.DateUtils
 import org.saudigitus.emis.data.local.DataManager
 import org.saudigitus.emis.data.model.Attendance
+import org.saudigitus.emis.data.model.dto.Absence
 import org.saudigitus.emis.data.model.dto.AttendanceEntity
 import org.saudigitus.emis.data.model.dto.withBtnSettings
+import org.saudigitus.emis.ui.components.Item
 import org.saudigitus.emis.ui.components.InfoCard
 import org.saudigitus.emis.ui.components.ToolbarHeaders
+import org.saudigitus.emis.utils.Constants.ABSENT
 import org.saudigitus.emis.utils.Constants.KEY
+import org.saudigitus.emis.utils.Constants.LATE
+import org.saudigitus.emis.utils.Constants.PRESENT
 import org.saudigitus.emis.utils.DateHelper
 import org.saudigitus.emis.utils.Utils.WHITE
+import org.saudigitus.emis.utils.Utils.getColorByAttendanceType
 import org.saudigitus.emis.utils.Utils.getColorByIconName
 import org.saudigitus.emis.utils.Utils.getDrawableIdByName
 import org.saudigitus.emis.utils.Utils.getIconByName
@@ -42,6 +48,9 @@ class AttendanceViewModel
     private val _attendanceStatus = MutableStateFlow<List<AttendanceEntity>>(emptyList())
     val attendanceStatus: StateFlow<List<AttendanceEntity>> = _attendanceStatus
 
+    private val _reasonOfAbsence = MutableStateFlow<List<Item>>(emptyList())
+    val reasonOfAbsence: StateFlow<List<Item>> = _reasonOfAbsence
+
     private val _attendanceStep = MutableStateFlow(ButtonStep.EDITING)
     val attendanceStep: StateFlow<ButtonStep> = _attendanceStep
 
@@ -49,14 +58,29 @@ class AttendanceViewModel
         MutableStateFlow<List<AttendanceActionButtonState>>(emptyList())
     val attendanceBtnState: StateFlow<List<AttendanceActionButtonState>> = _attendanceBtnState
 
-    private val _toolbarHeaders = MutableStateFlow(ToolbarHeaders(title = "Attendance"))
+    private val _toolbarHeaders = MutableStateFlow(ToolbarHeaders(
+        title = "Attendance",
+        subtitle = DateHelper.formatDateWithWeekDay("${DateHelper.formatDate(System.currentTimeMillis())}")
+    ))
     val toolbarHeaders: StateFlow<ToolbarHeaders> = _toolbarHeaders
 
     private val _program = MutableStateFlow("")
     val program: StateFlow<String> = _program
 
+    private val _eventDate = MutableStateFlow(DateHelper.formatDate(System.currentTimeMillis()) ?: "")
+    val eventDate: StateFlow<String> = _eventDate
+
     private val _infoCard = MutableStateFlow(InfoCard())
     val infoCard: StateFlow<InfoCard> = _infoCard
+
+    private val attendanceCache = mutableSetOf<AttendanceEntity>()
+    private val attendanceBtnStateCache = mutableListOf(AttendanceActionButtonState())
+
+    private val _absenceState = MutableStateFlow(Absence())
+    private val absenceState: StateFlow<Absence> = _absenceState
+
+    private val _absenceStateCache = MutableStateFlow<List<Absence>>(emptyList())
+    val absenceStateCache: StateFlow<List<Absence>> = _absenceStateCache
 
     init {
         viewModelScope.launch {
@@ -69,6 +93,7 @@ class AttendanceViewModel
                 }
             }
             getAttendanceOptions(datastoreAttendance.value?.status ?: "")
+            getReasonForAbsence(datastoreAttendance.value?.absenceReason ?: "")
         }
     }
 
@@ -87,7 +112,12 @@ class AttendanceViewModel
         _infoCard.value = infoCard
     }
 
+    fun setAttendanceStep(attendanceStep: ButtonStep) {
+        _attendanceStep.value = attendanceStep
+    }
+
     fun setAttendanceDate(date: String) {
+        _eventDate.value = date
         viewModelScope.launch {
             attendanceEvents(date)
         }
@@ -174,4 +204,141 @@ class AttendanceViewModel
             contentColor = WHITE
         )
     )
+
+    private fun getAttendanceUiState(
+        index: Int,
+        tei: String,
+        value: String
+    ): MutableList<AttendanceActionButtonState> {
+        val uiCache = attendanceBtnStateCache.find { it.btnId == tei }
+
+        val uiCacheItem = attendanceActionButtonMapper(
+            index = index,
+            tei = tei,
+            attendanceValue = value,
+            containerColor = getColorByAttendanceType(value)
+        )
+
+        if (uiCache == null) {
+            attendanceBtnStateCache.add(uiCacheItem)
+        } else {
+            attendanceBtnStateCache.remove(uiCache)
+            attendanceBtnStateCache.add(uiCacheItem)
+        }
+
+        return attendanceBtnStateCache
+    }
+
+    private fun getReasonForAbsence(dataElement: String) {
+        viewModelScope.launch {
+            _reasonOfAbsence.value = repository.getOptions(dataElement).map {
+                Item(
+                    id = it.uid(),
+                    itemName = "${it.displayName()}",
+                    code = it.code() ?: ""
+                )
+            }
+        }
+    }
+
+    fun setAttendance(
+        index: Int,
+        ou: String,
+        tei: String,
+        value: String,
+        reasonOfAbsence: String? = null
+    ) {
+        _attendanceBtnState.value = getAttendanceUiState(index, tei, value)
+
+        val attendance = AttendanceEntity(
+            tei = tei,
+            dataElement = datastoreAttendance.value?.status ?: "",
+            value = value,
+            reasonDataElement = datastoreAttendance.value?.absenceReason,
+            reasonOfAbsence = reasonOfAbsence,
+            date = eventDate.value
+        )
+
+        val cacheItem = attendanceCache.find { it.tei == attendance.tei }
+
+        if (cacheItem == null) {
+            attendanceCache.add(attendance)
+        } else {
+            attendanceCache.remove(cacheItem)
+            attendanceCache.add(attendance)
+        }
+
+        viewModelScope.launch {
+            repository.save(
+                ou = ou,
+                program = program.value,
+                programStage = datastoreAttendance.value?.programStage ?: "",
+                attendance = attendance
+            )
+        }
+    }
+
+    fun setAbsence(
+        index: Int? = null,
+        ou: String? = null,
+        tei: String? = null,
+        value: String? = null,
+        reasonOfAbsence: String? = null
+    ) {
+        if (index != null) {
+            _absenceState.update {
+                it.copy(index = index)
+            }
+        }
+        if (ou != null) {
+            _absenceState.update {
+                it.copy(ou = ou)
+            }
+        }
+        if (tei != null) {
+            _absenceState.update {
+                it.copy(tei = tei)
+            }
+        }
+        if (value != null) {
+            _absenceState.update {
+                it.copy(value = value)
+            }
+        }
+        if (reasonOfAbsence != null) {
+            _absenceState.update {
+                it.copy(reasonOfAbsence = reasonOfAbsence)
+            }
+        }
+    }
+    fun saveAbsenceState() {
+        setAttendance(
+            index = absenceState.value.index,
+            ou = absenceState.value.ou,
+            tei = absenceState.value.tei,
+            value = absenceState.value.value,
+            reasonOfAbsence = absenceState.value.reasonOfAbsence
+        )
+
+        val cache = mutableListOf<Absence>()
+
+        cache.addAll( absenceStateCache.value)
+        cache.add(absenceState.value)
+
+        _absenceStateCache.value = cache
+    }
+
+    fun getSummary(): Triple<String, String, String> {
+        val presence = "${attendanceCache.count { it.value == PRESENT }}"
+        val lateness = "${attendanceCache.count { it.value == LATE }}"
+        val absence = "${attendanceCache.count { it.value == ABSENT }}"
+
+        return Triple(presence, lateness, absence)
+    }
+
+    fun clearCache() {
+        attendanceCache.clear()
+        attendanceBtnStateCache.clear()
+        _absenceStateCache.value = emptyList()
+    }
 }
