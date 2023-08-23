ab4590ed2322243ffaf6dacea60adaed85fb2824
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-08-23 12:32:43 +0200

    file format and sending ou uid on data collect

--- emis/src/main/java/org/saudigitus/emis/ui/save_filter/SaveConfig.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/save_filter/SaveConfig.kt
@@ -10,25 +10,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.rounded.CleaningServices
 import androidx.compose.material.icons.rounded.Edit
-import androidx.compose.material.icons.rounded.School
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -45,13 +40,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.dhis2.commons.Constants
 import org.saudigitus.emis.R
-import org.saudigitus.emis.data.model.Favorite
 import org.saudigitus.emis.ui.components.DropDownOu
-import org.saudigitus.emis.ui.components.InfoCard
-import org.saudigitus.emis.ui.components.MetadataItem
 import org.saudigitus.emis.ui.components.SumaryCard
-import org.saudigitus.emis.ui.components.Toolbar
-import org.saudigitus.emis.ui.components.ToolbarActionState
 import org.saudigitus.emis.ui.teis.FilterType
 import org.saudigitus.emis.ui.teis.TeiViewModel
 import timber.log.Timber
@@ -73,20 +63,25 @@ fun SaveFavoriteFilterScreen(
     viewModel: TeiViewModel,
     favoriteViewModel: FavoriteViewModel,
     onBack: () -> Unit,
-    ) {
+) {
 
     val filterState by viewModel.filterState.collectAsStateWithLifecycle()
     val programSettings by viewModel.programSettings.collectAsStateWithLifecycle()
     val dataElementFilters by viewModel.dataElementFilters.collectAsStateWithLifecycle()
     val favorites by favoriteViewModel.favorites.collectAsStateWithLifecycle()
 
-    val selectedStatesGrade = remember {List(dataElementFilters[FilterType.GRADE]!!.size) { mutableStateOf(false) } }
-    val selectedStatesSection = remember {List(dataElementFilters[FilterType.SECTION]!!.size) { mutableStateOf(false) } }.toMutableList()
+    val selectedStatesGrade =
+        remember { List(dataElementFilters[FilterType.GRADE]!!.size) { mutableStateOf(false) } }
+    val selectedStatesSection =
+        remember { List(dataElementFilters[FilterType.SECTION]!!.size) { mutableStateOf(false) } }.toMutableList()
 
     Timber.tag("MY_FAVORITES").e("$favorites")
 
-    if(filterState.school?.uid != null){
-        favoriteViewModel.setFavorite(school = filterState.school?.displayName)
+    if (filterState.school?.uid != null) {
+        favoriteViewModel.setFavorite(
+            schoolUid = filterState.school?.uid!!,
+            school = filterState.school!!.displayName
+        )
     }
 
     Scaffold(
@@ -108,11 +103,12 @@ fun SaveFavoriteFilterScreen(
             )
         },
         floatingActionButton = {
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement =  Arrangement.SpaceBetween
-            ){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 ExtendedFloatingActionButton(
-                    modifier = Modifier.padding(horizontal =  32.dp),
+                    modifier = Modifier.padding(horizontal = 32.dp),
                     text = {
                         Text(
                             text = "Reset",
@@ -156,88 +152,94 @@ fun SaveFavoriteFilterScreen(
                 )
             }
         }
-        ){ paddingValues -> Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start
-        ){
-           Column {
-               Spacer(modifier = Modifier.size(20.dp))
-               DropDownOu(
-                   placeholder = stringResource(R.string.school),
-                   leadingIcon = ImageVector.vectorResource(R.drawable.ic_location_on),
-                   selectedSchool = filterState.school,
-                   program = programSettings?.getString(Constants.PROGRAM_UID) ?: " ",
-                   onItemClick = {
-                       viewModel.setSchool(it)
-                       favoriteViewModel.setFavorite(school = filterState.school?.displayName)
-                   }
-               )
-               Column(modifier = Modifier
-                   .fillMaxWidth()
-                   .padding(16.dp)){
-                   Spacer(modifier = Modifier.size(10.dp))
-                   Text("Grade", color = Color.Gray)
-                   Spacer(modifier = Modifier.size(10.dp))
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Column {
+                Spacer(modifier = Modifier.size(20.dp))
+                DropDownOu(
+                    placeholder = stringResource(R.string.school),
+                    leadingIcon = ImageVector.vectorResource(R.drawable.ic_location_on),
+                    selectedSchool = filterState.school,
+                    program = programSettings?.getString(Constants.PROGRAM_UID) ?: " ",
+                    onItemClick = {
+                        viewModel.setSchool(it)
+                        favoriteViewModel.setFavorite(
+                            schoolUid = filterState.school?.uid ?: "",
+                            school = filterState.school?.displayName ?: ""
+                        )
+                    }
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text("Grade", color = Color.Gray)
+                    Spacer(modifier = Modifier.size(10.dp))
 
-                   LazyRow(){
-                       itemsIndexed(dataElementFilters[FilterType.GRADE]!!) { index, grade ->
-                           TextChipWithIconVisibility(
-                               isSelected = selectedStatesGrade[index],
-                               "${grade.itemName}",
-                               "${grade.code}",
-                           ) { checked, code ->
-                               selectedStatesGrade[index].value = checked // Update the MutableState value
-                               println("CHECKED: ${checked} GRADE CODE: $code")
-                               favoriteViewModel.setFavorite(gradeCode = code)
-                           }
-                       }
-                   }
-                   Spacer(modifier = Modifier.size(10.dp))
-                   Text("Class/Section", color = Color.Gray)
-                   Spacer(modifier = Modifier.size(10.dp))
-                   LazyRow(){
-                       itemsIndexed(dataElementFilters[FilterType.SECTION]!!) {
-                               index, section ->
-                           TextChipWithIconVisibility(
-                               isSelected = selectedStatesSection[index],
-                               "${section.itemName}",
-                               "${section.code}",
-                           ) { checked, code ->
-                               //selectedStatesSection[index] = checked
-                               println("CHECKED: ${checked} SECTION CODE: $code")
-                               favoriteViewModel.setFavorite(sectionCode = code)
-                           }
-                       }
-                   }
-                   Spacer(modifier = Modifier.size(10.dp))
-                   Text("Summary", color = Color.Gray)
-                   /*SumaryCard(
-                       //InfoCard(),
-                   )*/
+                    LazyRow {
+                        itemsIndexed(dataElementFilters[FilterType.GRADE]!!) { index, grade ->
+                            TextChipWithIconVisibility(
+                                isSelected = selectedStatesGrade[index],
+                                "${grade.itemName}",
+                                "${grade.code}",
+                            ) { checked, code ->
+                                selectedStatesGrade[index].value =
+                                    checked // Update the MutableState value
+                                println("CHECKED: ${checked} GRADE CODE: $code")
+                                favoriteViewModel.setFavorite(gradeCode = code)
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text("Class/Section", color = Color.Gray)
+                    Spacer(modifier = Modifier.size(10.dp))
+                    LazyRow {
+                        itemsIndexed(dataElementFilters[FilterType.SECTION]!!) { index, section ->
+                            TextChipWithIconVisibility(
+                                isSelected = selectedStatesSection[index],
+                                "${section.itemName}",
+                                "${section.code}",
+                            ) { checked, code ->
+                                //selectedStatesSection[index] = checked
+                                println("CHECKED: ${checked} SECTION CODE: $code")
+                                favoriteViewModel.setFavorite(sectionCode = code)
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text("Summary", color = Color.Gray)
+                    /*SumaryCard(
+                        //InfoCard(),
+                    )*/
 
-                   if(favorites.isEmpty()){
-                       Text("Please select your favourite Grades and classes",
-                           modifier = Modifier.padding(16.dp),
-                           color = Color.Gray,
-                           fontSize = 12.sp)
-                   }
-                   else {
-                       LazyColumn{
-                           itemsIndexed(favorites) {
-                                   index, favorite ->
-                               SumaryCard(
-                                  school = favorite.school,
-                                  streams = favorite.stream
-                               )
-                           }
-                       }
-                   }
-               }
+                    if (favorites.isEmpty()) {
+                        Text(
+                            "Please select your favourite Grades and classes",
+                            modifier = Modifier.padding(16.dp),
+                            color = Color.Gray,
+                            fontSize = 12.sp
+                        )
+                    } else {
+                        LazyColumn {
+                            itemsIndexed(favorites) { index, favorite ->
+                                SumaryCard(
+                                    school = favorite.school,
+                                    streams = favorite.stream
+                                )
+                            }
+                        }
+                    }
+                }
 
-           }
+            }
         }
     }
 }
