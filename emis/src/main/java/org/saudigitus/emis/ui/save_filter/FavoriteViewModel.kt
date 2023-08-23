4befb592789437440340fa0fb1a1afea0ce59c39
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-08-23 12:33:46 +0200

    improving favorite data collect and saving method

--- emis/src/main/java/org/saudigitus/emis/ui/save_filter/FavoriteViewModel.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/save_filter/FavoriteViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.saudigitus.emis.data.local.FavoriteConfigRepository
@@ -27,11 +28,11 @@ class FavoriteViewModel
     private val _favorite = MutableStateFlow(Favorite())
     val favorite: StateFlow<Favorite> = _favorite
 
-    private val _streams = MutableStateFlow<List<Stream>>(emptyList())
-    val streams: StateFlow<List<Stream>> = _streams
+    private val _stream = MutableStateFlow(Stream())
+    private val stream: StateFlow<Stream> = _stream
 
     private val _sections = MutableStateFlow<List<Section>>(emptyList())
-    val sections: StateFlow<List<Section>> = _sections
+    private val sections: StateFlow<List<Section>> = _sections
 
 
     init {
@@ -39,6 +40,7 @@ class FavoriteViewModel
     }
 
     fun setFavorite(
+        schoolUid: String = "",
         school: String? = null,
         gradeCode: String? = null,
         sectionCode: String? = null,
@@ -46,32 +48,35 @@ class FavoriteViewModel
     ){
         if(school != null) {
             _favorite.update {
-                it.copy(school = school)
+                it.copy(uid = schoolUid, school = school)
             }
         }
 
-        val junkSections = mutableListOf<Section>()
         if(sectionCode != null) {
+            val junkSections = mutableListOf<Section>()
+            val junkStream = mutableListOf<Stream>()
+
             junkSections.addAll(sections.value)
             junkSections.add(Section(sectionCode, sectionName))
             _sections.value = junkSections
 
-            println("SECTIONN J $junkSections")
-            println("SECTIONN O ${sections.value}")
-        }
+            _stream.update {
+                it.copy(sections = sections.value)
+            }
 
-        val junkStream = mutableListOf<Stream>()
-        if(gradeCode != null) {
-            junkStream.addAll(streams.value)
-            junkStream.add(Stream(gradeCode, junkSections))
-            _streams.value = junkStream
+            val record = favorites.value.find { it.uid == favorite.value.uid }
 
-            println("STREAM OK ${streams.value}")
-            println("STREAM OHK $junkStream")
-            println("SECTIONN O2 ${sections.value}")
+            record?.stream?.let { junkStream.addAll(it) }
+            junkStream.add(stream.value)
 
             _favorite.update {
-                it.copy(stream = streams.value)
+                it.copy(stream = junkStream)
+            }
+        }
+
+        if(gradeCode != null) {
+            _stream.update {
+                it.copy(grade = gradeCode)
             }
         }
     }
@@ -90,14 +95,14 @@ class FavoriteViewModel
             favoritesCache.add(favorite.value)
 
             repository.save(FavoriteConfig(favoritesCache))
-            Timber.tag("FAVORITE_savE").e("${favorite.value}")
 
+            Timber.tag("FAVORITE_savE").e("${favorite.value}")
         }
     }
 
     private fun getFavorites() {
         viewModelScope.launch {
-            repository.getFavorites().collect {
+            repository.getFavorites().collectLatest {
                 _favorites.value = it.favorites ?: emptyList()
             }
         }
