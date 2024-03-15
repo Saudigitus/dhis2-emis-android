18622a5fb262dc5607a6c0ea4d52e7e59f9f32a7
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-03-15 14:15:46 +0200

    form repository dependency injection

--- emis/src/main/java/org/saudigitus/emis/di/AppModule.kt
+++ emis/src/main/java/org/saudigitus/emis/di/AppModule.kt
@@ -7,11 +7,15 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import org.dhis2.commons.network.NetworkUtils
+import org.dhis2.form.ui.provider.HintProvider
+import org.dhis2.form.ui.provider.HintProviderImpl
 import org.hisp.dhis.android.core.D2
 import org.saudigitus.emis.data.local.DataManager
 import org.saudigitus.emis.data.local.FavoriteConfigRepository
+import org.saudigitus.emis.data.local.FormRepository
 import org.saudigitus.emis.data.local.repository.DataManagerImpl
 import org.saudigitus.emis.data.local.repository.FavoriteConfigRepositoryImpl
+import org.saudigitus.emis.data.local.repository.FormRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -30,6 +34,17 @@ object AppModule {
         d2: D2,
         networkUtils: NetworkUtils
     ): DataManager = DataManagerImpl(d2, networkUtils)
+
+    @Provides
+    @Singleton
+    fun providesHintProvider(@ApplicationContext context: Context): HintProvider =
+        HintProviderImpl(context)
+
+    @Provides
+    @Singleton
+    fun providesFormRepository(d2: D2, hintProvider: HintProvider): FormRepository {
+        return FormRepositoryImpl(d2, hintProvider)
+    }
 
     @Provides
     @Singleton
