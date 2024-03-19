1575ffe075bcac9c93f3207179c1a5aa4a72fa68
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-03-19 09:10:12 +0200

    centering text in field and increasing font size

--- emis/src/main/java/org/saudigitus/emis/ui/form/InputField.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/form/InputField.kt
@@ -3,24 +3,21 @@ package org.saudigitus.emis.ui.form
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import org.dhis2.composetable.model.extensions.keyboardCapitalization
 import org.dhis2.composetable.model.extensions.toKeyboardType
 import org.hisp.dhis.android.core.common.ValueType
-import org.saudigitus.emis.R
 import org.saudigitus.emis.utils.toKeyBoardInputType
 
 @Composable
@@ -52,6 +49,7 @@ fun InputField(
             PasswordVisualTransformation()
         } else {
             VisualTransformation.None
-        }
+        },
+        textStyle = TextStyle(fontSize = 25.sp, textAlign = TextAlign.Center)
     )
 }
