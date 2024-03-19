fdec393d12a652abe95ca25896518e2680204abd
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-03-19 09:05:27 +0200

    adding marks routes

--- emis/src/main/java/org/saudigitus/emis/AppRoutes.kt
+++ emis/src/main/java/org/saudigitus/emis/AppRoutes.kt
@@ -3,6 +3,5 @@ package org.saudigitus.emis
 object AppRoutes {
     const val TEI_LIST_ROUTE = "tei_listing"
     const val ATTENDANCE_ROUTE = "attendance"
-    const val FAVORITE_ROUTE = "favorite"
-
+    const val MARKS_ROUTE = "marks"
 }
