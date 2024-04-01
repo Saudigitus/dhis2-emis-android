46c4fb905ce3fe9497657447816f741847bf5664
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-01 10:23:04 +0200

    model rename and filtering fields by dataelement

--- emis/src/main/java/org/saudigitus/emis/data/local/FormRepository.kt
+++ emis/src/main/java/org/saudigitus/emis/data/local/FormRepository.kt
@@ -2,13 +2,13 @@ package org.saudigitus.emis.data.local
 
 
 import org.hisp.dhis.android.core.option.Option
-import org.saudigitus.emis.data.model.Saving
+import org.saudigitus.emis.data.model.EventTuple
 import org.saudigitus.emis.ui.form.FormData
 import org.saudigitus.emis.ui.form.FormField
 
 interface FormRepository {
-    suspend fun save(saving: Saving)
-    suspend fun keyboardInputTypeByStage(stage: String): List<FormField>?
+    suspend fun save(eventTuple: EventTuple)
+    suspend fun keyboardInputTypeByStage(stage: String, dl: String): List<FormField>?
     suspend fun getOptions(dataElement: String): List<Option>
     suspend fun getEventsByDate(
         ou: String,
