b7a8e8da50f890d3e2c25e37c12d801746789348
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-01 10:25:30 +0200

    implementing subjects and marks route

--- emis/src/main/java/org/saudigitus/emis/MainActivity.kt
+++ emis/src/main/java/org/saudigitus/emis/MainActivity.kt
@@ -11,15 +11,15 @@ import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
 import org.dhis2.commons.Constants
 import org.saudigitus.emis.ui.attendance.AttendanceScreen
 import org.saudigitus.emis.ui.attendance.AttendanceViewModel
-import org.saudigitus.emis.ui.favorites.FavoriteViewModel
-import org.saudigitus.emis.ui.favorites.SaveFavoriteFilterScreen
 import org.saudigitus.emis.ui.marks.MarksScreen
 import org.saudigitus.emis.ui.marks.MarksViewModel
 import org.saudigitus.emis.ui.subjects.SubjectScreen
@@ -71,28 +71,6 @@ class MainActivity : FragmentActivity() {
                                 navController.navigateUp()
                             }
                         }
-                        composable(AppRoutes.MARKS_ROUTE) {
-                            val marksViewModel = hiltViewModel<MarksViewModel>()
-                            val uiState by marksViewModel.uiState.collectAsStateWithLifecycle()
-                            val infoCard by marksViewModel.infoCard.collectAsStateWithLifecycle()
-
-                            marksViewModel.setProgram(intent?.extras?.getString(Constants.PROGRAM_UID) ?: "")
-                            marksViewModel.setTeis(
-                                viewModel.teis.collectAsStateWithLifecycle().value,
-                                marksViewModel::updateTEISList
-                            )
-                            marksViewModel.setInfoCard(viewModel.infoCard.collectAsStateWithLifecycle().value)
-
-                            MarksScreen(
-                                state = uiState,
-                                onNavBack = navController::navigateUp,
-                                infoCard = infoCard,
-                                setMarksState = marksViewModel::marksState,
-                                setDate = marksViewModel::setDate,
-                                onNext = marksViewModel::onClickNext,
-                                onSave = marksViewModel::save
-                            )
-                        }
                         composable(AppRoutes.SUBJECT_ROUTE) {
                             val subjectViewModel = hiltViewModel<SubjectViewModel>()
                             val state by subjectViewModel.uiState.collectAsStateWithLifecycle()
@@ -102,7 +80,47 @@ class MainActivity : FragmentActivity() {
                                 state = state,
                                 onBack = navController::navigateUp,
                                 onFilterClick = subjectViewModel::performOnFilterClick,
-                                onClick = subjectViewModel::performOnClick
+                                onClick = { stage, dl ->
+                                    navController.navigate("${AppRoutes.MARKS_ROUTE}/$stage/$dl")
+                                }
+                            )
+                        }
+                        composable(
+                            route = "${AppRoutes.MARKS_ROUTE}/{stage}/{dataElement}",
+                            arguments =  listOf(
+                                navArgument("stage") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dataElement") {
+                                    type = NavType.StringType
+                                },
+                            )
+                        ) {
+                            val stage = it.arguments?.getString("stage") ?: ""
+                            val dl = it.arguments?.getString("dataElement") ?: ""
+
+                            val marksViewModel = hiltViewModel<MarksViewModel>()
+                            val uiState by marksViewModel.uiState.collectAsStateWithLifecycle()
+                            val infoCard by viewModel.infoCard.collectAsStateWithLifecycle()
+                            val stats by marksViewModel.marksCache.collectAsStateWithLifecycle()
+                            val teis by viewModel.teis.collectAsStateWithLifecycle()
+                            val marksStep by marksViewModel.buttonStep.collectAsStateWithLifecycle()
+
+                            marksViewModel.setProgram(intent?.extras?.getString(Constants.PROGRAM_UID) ?: "")
+                            marksViewModel.setTeis(teis, marksViewModel::updateTEISList)
+                            marksViewModel.getFields(stage, dl)
+
+                            MarksScreen(
+                                state = uiState,
+                                onNavBack = navController::navigateUp,
+                                infoCard = infoCard,
+                                setMarksState = marksViewModel::marksState,
+                                markStats = Pair("${stats.size}", "${teis.size.minus(stats.size)}"),
+                                setDate = marksViewModel::setDate,
+                                onNext = marksViewModel::onClickNext,
+                                marksStep = marksStep,
+                                step = marksViewModel::setButtonStep,
+                                onSave = marksViewModel::save
                             )
                         }
                     }
