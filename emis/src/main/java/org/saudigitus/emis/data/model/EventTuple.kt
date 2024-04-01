668519b1b8e2cb5e2ad40879bf8f3dcd70fcc734
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-01 10:21:54 +0200

    model rename

--- emis/src/main/java/org/saudigitus/emis/data/model/EventTuple.kt
+++ emis/src/main/java/org/saudigitus/emis/data/model/EventTuple.kt
@@ -2,7 +2,7 @@ package org.saudigitus.emis.data.model
 
 import org.dhis2.form.model.RowAction
 
-data class Saving(
+data class EventTuple(
     val ou: String,
     val program: String,
     val programStage: String,
