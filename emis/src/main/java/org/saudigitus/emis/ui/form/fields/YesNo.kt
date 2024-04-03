9b7308731ca335b4849f105b8e84eece60c9ff70
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-03 12:23:24 +0200

    adding form fields

---
+++ emis/src/main/java/org/saudigitus/emis/ui/form/fields/YesNo.kt
@@ -0,0 +1,13 @@
+package org.saudigitus.emis.ui.form.fields
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun YesNo() {
+    Row {
+        RadioButton(selected =, onClick = { /*TODO*/ })
+        RadioButton(selected =, onClick = { /*TODO*/ })
+    }
+}
