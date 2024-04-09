4284697cf4b1633abfc726b0049cc8aa0718efe8
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-09 16:44:27 +0200

    update

--- emis/src/main/java/org/saudigitus/emis/ui/attendance/AttendanceDialogs.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/attendance/AttendanceDialogs.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -38,6 +39,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -198,7 +200,7 @@ fun AttendanceSummaryDialog(
 
 
 @Composable
-private fun SummaryComponent(
+fun SummaryComponent(
     title: String? = null,
     summary: String,
     containerColor: Color,
@@ -226,6 +228,42 @@ private fun SummaryComponent(
                 imageVector = icon,
                 contentDescription = title,
                 tint = Color.White
+            )
+            Text(text = summary)
+        }
+    }
+}
+
+@Composable
+fun SummaryComponent(
+    title: String? = null,
+    summary: String,
+    containerColor: Color,
+    icon: Painter
+) {
+    Card(
+        modifier = Modifier
+            .width(95.dp)
+            .padding(10.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = containerColor,
+            contentColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = title,
+                tint = Color.White,
+                modifier = Modifier.size(24.dp)
             )
             Text(text = summary)
         }
@@ -273,7 +311,7 @@ private fun DialogTemplate(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun AlertDialogTemplate(
+fun AlertDialogTemplate(
     content: @Composable() (ColumnScope.() -> Unit)
 ) {
     AlertDialog(
