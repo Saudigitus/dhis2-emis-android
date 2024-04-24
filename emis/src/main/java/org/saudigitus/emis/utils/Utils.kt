5b0e404fd467b2e62e73d1756b8ef672f2c1fc0a
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-24 10:27:59 +0200

    feat: implement dynamic icons

--- emis/src/main/java/org/saudigitus/emis/utils/Utils.kt
+++ emis/src/main/java/org/saudigitus/emis/utils/Utils.kt
@@ -1,33 +1,17 @@
 package org.saudigitus.emis.utils
 
-import android.annotation.SuppressLint
-import android.content.Context
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
 import org.saudigitus.emis.R
 import org.saudigitus.emis.utils.Constants.ABSENT
 import org.saudigitus.emis.utils.Constants.LATE
 import org.saudigitus.emis.utils.Constants.PRESENT
 
 object Utils {
-    @SuppressLint("DiscouragedApi")
-    fun getDrawableIdByName(
-        context: Context,
-        name: String
-    )  = context.resources
-        .getIdentifier(
-            name,
-            "drawable",
-            context.packageName
-        )
-
-    /**
-     * This functions return specific icons by its name...
-     */
-    fun getDrawableIdByName(name: String) = when (name) {
-        "correct_blue_fill" -> R.drawable.correct_blue_fill
-        "wrong_red_fill" -> R.drawable.wrong_red_fill
-        "clock_orange_fill" -> R.drawable.clock_orange_fill
-        else -> R.drawable.ic_empty
-    }
+    const val GREEN = 0xFF81C784
+    const val RED = 0xFFE57373
+    const val ORANGE = 0xFFFFB74D
+    const val WHITE = 0xFFFFFFFF
 
     fun getIconByName(name: String) = when (name) {
         "correct_blue_fill" -> R.drawable.present
@@ -36,18 +20,13 @@ object Utils {
         else -> R.drawable.ic_empty
     }
 
-    private const val GREEN = 0xFF81C784
-    private const val RED = 0xFFE57373
-    private const val ORANGE = 0xFFFFB74D
-    const val WHITE = 0xFFFFFFFF
-
-    fun getColorByIconName(name: String) = when (name) {
-        "correct_blue_fill" -> GREEN
-        "wrong_red_fill" -> RED
-        "clock_orange_fill" -> ORANGE
-        else -> WHITE
+    fun dynamicIcons(name: String) = try {
+        val cl = Class.forName("androidx.compose.material.icons.filled.${name}Kt")
+        val method = cl.declaredMethods.first()
+        method.invoke(null, Icons.Filled) as ImageVector
+    } catch (_: Throwable) {
+        null
     }
-
 
     fun getColorByAttendanceType(type: String) = when (type.lowercase()) {
         PRESENT -> { GREEN }
