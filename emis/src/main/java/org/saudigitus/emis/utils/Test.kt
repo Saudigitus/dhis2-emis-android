f63f0477a555567c62639f827c513da206819422
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-04-24 10:28:29 +0200

    feat: implement mocked test case

--- emis/src/main/java/org/saudigitus/emis/utils/Test.kt
+++ emis/src/main/java/org/saudigitus/emis/utils/Test.kt
@@ -1,9 +1,13 @@
 package org.saudigitus.emis.utils
 
+import org.saudigitus.emis.R
 import org.saudigitus.emis.data.model.CalendarConfig
 import org.saudigitus.emis.data.model.ClassPeriod
 import org.saudigitus.emis.data.model.Holiday
 import org.saudigitus.emis.data.model.WeekDays
+import org.saudigitus.emis.utils.Utils.GREEN
+import org.saudigitus.emis.utils.Utils.ORANGE
+import org.saudigitus.emis.utils.Utils.RED
 
 object Test {
 
@@ -29,5 +33,11 @@ object Test {
             saturday = false,
             sunday = false
         )
+    )
+
+    val attendanceStatus = listOf(
+        Triple(R.drawable.present, "Present", GREEN),
+        Triple(R.drawable.late, "Late", ORANGE),
+        Triple(R.drawable.absent, "Absent", RED),
     )
 }
